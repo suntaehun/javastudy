c7d286f61a0db86578f84bc522cdd94184e9d481
suntaehun <sth7862@naver.com>
2022-10-20 10:59:03 +0900

    Ex01Variable

---
+++ src/main/java/chapter01/Ex01Variable.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+public class Ex01Variable {
+
+	public static void main(String[] args) {
+		int i = 1000;
+		String s = "Hello World!";
+		
+		System.out.println(i);
+		System.out.println(s);
+
+	}
+
+}
