34cb2549d409d0953af33af366264cf308e3f73e
suntaehun <sth7862@naver.com>
2022-10-26 15:26:57 +0900

    오버라이딩

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -4,19 +4,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Point pt = new Point(10, 50);
-//		pt.setX(10);
-//		pt.setY(50);
+		Point pt1 = new Point(10, 50);
+//		pt1.setX(10);
+//		pt1.setY(50);
 		
-		drawPoint(pt);
+		drawPoint(pt1);
 		
-//		pt.disappear();
-		pt.show(false);
+//		pt1.disappear();
+		pt1.show(false);
+		
+//		ColorPoint pt2 = new ColorPoint();
+		Point pt2 = new ColorPoint();
+		pt2.setX(100);
+		pt2.setY(200);
+//		pt2.setColor("red");
+		((ColorPoint)pt2).setColor("red");
+		
+//		drawColorPoint(pt2);
+		drawPoint(pt2);
+		pt2.show(false);
+		pt2.show(true);
 		
 	}
 	
 	public static void drawPoint(Point pt) {
 		pt.show();
 	}
+	
+//	public static void drawColorPoint(ColorPoint pt) {
+//		pt.show();
+//	}
 	
 }
