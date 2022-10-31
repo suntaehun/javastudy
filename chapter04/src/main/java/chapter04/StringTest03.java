38a1635042f2627e85c70afff1ef80676d5afb78
suntaehun <sth7862@naver.com>
2022-10-31 13:59:36 +0900

    String

---
+++ chapter04/src/main/java/chapter04/StringTest03.java
@@ -0,0 +1,35 @@
+package chapter04;
+
+public class StringTest03 {
+
+	public static void main(String[] args) {
+		// String method 들...
+		String s1 = "aBcABCabcAbc";
+		System.out.println(s1.length());
+		System.out.println(s1.charAt(2));
+		System.out.println(s1.indexOf("abc"));
+		System.out.println(s1.indexOf("abc",7));
+		System.out.println(s1.substring(3));
+		System.out.println(s1.substring(3,5));
+		
+		String s2 = "   ab   cd   ";
+		String s3 = "efg,hij,klm,nop,qrs";
+		
+		String s4 = s1.concat(s3);
+		System.out.println(s4);
+		
+		System.out.println("-----" + s2.trim() + "-----");
+		System.out.println("-----" + s2.replaceAll(" ", "") + "-----");
+		
+		String[] tokens = s3.split(",");
+		for(String s:tokens) {
+			System.out.println(s);
+		}
+		
+		String[] tokens2 = s3.split(" ");
+		for(String s:tokens2) {
+			System.out.println(s);
+		}
+	}
+
+}
---
+++ chapter04/src/main/java/chapter04/StringTest04.java
@@ -0,0 +1,27 @@
+package chapter04;
+
+public class StringTest04 {
+
+	public static void main(String[] args) {
+//		String s1 = "Hello " + "World " + "Java " + 12;
+		String s1 = 
+				new StringBuffer("Hello ").append("World ").append("Java ").append(12).toString();
+		
+		System.out.println(s1);
+		
+//		String s2 = "";
+//		for(int i = 0; i <= 100000; i++) {
+//			s2 = s2 + i;
+//			s2 = new StringBuffer(s2).append(i).toString();
+//		}
+
+		StringBuffer sb2 = new StringBuffer("");
+		for(int i = 0; i <= 100000; i++) {
+			sb2.append(i);
+		}
+		
+		String s2 = sb2.toString();
+		System.out.println(s2.length());
+	}
+
+}
