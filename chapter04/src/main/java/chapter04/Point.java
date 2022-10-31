9bad33063b013b85e1b6b52aff8c60bf24cbf356
suntaehun <sth7862@naver.com>
2022-10-31 10:50:47 +0900

    Hash

--- chapter04/src/main/java/chapter04/Point.java
+++ chapter04/src/main/java/chapter04/Point.java
@@ -1,5 +1,7 @@
 package chapter04;
 
+import java.util.Objects;
+
 public class Point {
 	private int x;
 	private int y;
@@ -13,6 +15,23 @@ public class Point {
 	public String toString() {
 		// TODO Auto-generated method stub
 		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);	// 31*x + 30*y
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		return x == other.x && y == other.y;
 	}
 	
 }
