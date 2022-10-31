875a9faaa699d1b64db387fe34c56ea608a0bd73
suntaehun <sth7862@naver.com>
2022-10-31 11:56:17 +0900

    Object

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,25 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+		
+		// "hello"
+		System.out.println("\"hello\"");
+		
+		// \t : tab
+		// \r : carrgie return
+		// \n : new line
+		// \b : beep
+		System.out.print("hello\tworld\n");
+		System.out.print("hello world");
+		
+		// '
+		char c = '\'';
+		System.out.println(c);
+
+	}
+
+}
